3cec41105389b18f79348fbcadef573694253c50
razareyaz408-collab <razareyaz408@gmail.com>
2026-07-09 00:50:19 +0530

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,1 +1,9 @@
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
+const supabaseUrl = "https://kkldkdjuyfydrfvwogol.supabase.co";
+
+const supabaseKey = "sb_publishable_Tgy7-sdCHWQpMD8qAUvc8A_o4OfjiJO";
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
