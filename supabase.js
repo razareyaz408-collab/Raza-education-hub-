7d397a55d01663c52483c6f962793be948b87a78
razareyaz408-collab <razareyaz408@gmail.com>
2026-07-11 17:48:16 +0530

    Delete supabase.js

--- supabase.js
+++
@@ -1,9 +0,0 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
-
-const supabaseUrl = "https://kkldkdjuyfydrfvwogol.supabase.co";
-
-const supabaseKey = "sb_publishable_MNQxLMEA-iiEzMo8lLZ0MA_EuKqTIC8";
-
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-export default supabase;
