20769e58e177ddd672e28f235fcf5d3b21080354
razareyaz408-collab <razareyaz408@gmail.com>
2026-07-11 23:56:08 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCNoVNUVqolCBP5SgXEHVSxy0VkT4IBZ24",
+  authDomain: "raza-education-hub.firebaseapp.com",
+  projectId: "raza-education-hub",
+  storageBucket: "raza-education-hub.firebasestorage.app",
+  messagingSenderId: "54645861151",
+  appId: "1:54645861151:web:908c60ae16dd80ba619e2a",
+  measurementId: "G-N0D06HSX8S"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
