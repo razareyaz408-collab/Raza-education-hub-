7c59aa75c75ba37c6a631a098e837db9958cd1a6
razareyaz408-collab <razareyaz408@gmail.com>
2026-07-12 21:45:52 +0530

    Update surah.js

--- surah.js
+++ surah.js
@@ -1,87 +1,54 @@
 const params = new URLSearchParams(window.location.search);
-
 const id = params.get("id");
 
 async function loadSurah() {
 
-try {
+  try {
 
-const response = await fetch(
-`https://api.alquran.cloud/v1/surah/${id}/editions/quran-uthmani,en.asad`
-);
+    const response = await fetch(
+      `https://api.alquran.cloud/v1/surah/${id}/editions/quran-uthmani,en.asad`
+    );
 
-const result = await response.json();
+    const result = await response.json();
 
-document.getElementById("surahTitle").innerHTML =
-arabic.englishName + " (" + arabic.name + ")";
+    const arabic = result.data[0];
+    const english = result.data[1];
 
-let html = "";
+    document.getElementById("surahTitle").innerHTML =
+      arabic.englishName + " (" + arabic.name + ")";
 
-const arabic = result.data[0];
-const english = result.data[1];
+    let html = "";
 
-arabic.ayahs.forEach((ayah, index) => {
+    arabic.ayahs.forEach((ayah, index) => {
 
-html += `
+      html += `
+      <div class="card">
 
-<div class="card">
+        <h2 style="text-align:right;font-size:32px;line-height:2;">
+          ${ayah.text}
+        </h2>
 
-<h2 style="text-align:right;font-size:32px;line-height:2;">
+        <p style="margin-top:15px;color:#444;">
+          ${english.ayahs[index].text}
+        </p>
 
-${ayah.text}
+        <p>Ayah ${ayah.numberInSurah}</p>
 
-</h2>
+      </div>
+      `;
 
-<p style="margin-top:15px;color:#444;">
+    });
 
-${english.ayahs[index].text}
+    document.getElementById("surahContent").innerHTML = html;
 
-</p>
+  } catch (error) {
 
-<p>
+    console.log(error);
 
-Ayah ${ayah.numberInSurah}
+    document.getElementById("surahContent").innerHTML =
+      "Failed to load Surah.";
 
-</p>
-
-</div>
-
-`;
-
-}); NJN
-
-html += `
-
-<div class="card">
-
-<h2 style="text-align:right;font-size:32px;">
-
-${ayah.text}
-
-</h2>
-
-<p>
-
-Ayah ${ayah.numberInSurah}
-
-</p>
-
-</div>
-
-`;
-
-});
-
-document.getElementById("surahContent").innerHTML = html;
-
-}
-
-catch(error){
-
-document.getElementById("surahContent").innerHTML =
-"Failed to load Surah.";
-
-}
+  }
 
 }
 
