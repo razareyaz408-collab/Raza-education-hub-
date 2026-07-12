426556b7136700d85440f44635644aac7d331b60
razareyaz408-collab <razareyaz408@gmail.com>
2026-07-12 06:24:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
+import { auth } from "./firebase.js";
 
+import {
+createUserWithEmailAndPassword,
+updateProfile
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+const signupForm = document.getElementById("signupForm");
+
+if (signupForm) {
+
+signupForm.addEventListener("submit", async (e) => {
+
+e.preventDefault();
+
+const name = document.getElementById("name").value.trim();
+
+const email = document.getElementById("email").value.trim();
+
+const password = document.getElementById("password").value;
+
+try {
+
+const userCredential = await createUserWithEmailAndPassword(
+auth,
+email,
+password
+);
+
+await updateProfile(userCredential.user, {
+displayName: name
+});
+
+alert("✅ Account Created Successfully!");
+
+window.location.href = "dashboard.html";
+
+}
+
+catch(error){
+
+alert(error.message);
+
+}
+
+});
+
+}
