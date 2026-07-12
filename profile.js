7d61abdfd82d159d05df4e14a3793d9a72bf79be
razareyaz408-collab <razareyaz408@gmail.com>
2026-07-13 00:02:01 +0530

    Update profile.js

--- profile.js
+++ profile.js
@@ -1,19 +1,22 @@
 import { auth } from "./firebase.js";
 
 import {
-
 onAuthStateChanged,
 signOut
-
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-onAuthStateChanged(auth,(user)=>{
+onAuthStateChanged(auth, (user) => {
 
 if(user){
 
-document.getElementById("userName").innerHTML=user.displayName || "Student";
+document.getElementById("userName").textContent =
+user.displayName || "Student";
 
-document.getElementById("userEmail").innerHTML=user.email;
+document.getElementById("userEmail").textContent =
+user.email;
+
+document.getElementById("userUID").textContent =
+user.uid;
 
 }else{
 
@@ -26,6 +29,8 @@ window.location.href="login.html";
 document.getElementById("logoutBtn").addEventListener("click",async()=>{
 
 await signOut(auth);
+
+alert("Logout Successful");
 
 window.location.href="login.html";
 
