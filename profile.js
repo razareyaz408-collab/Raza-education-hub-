f2fb5a35e6e85e4f622e700cbe78c43c419523cf
razareyaz408-collab <razareyaz408@gmail.com>
2026-07-13 00:13:37 +0530

    Update profile.js

--- profile.js
+++ profile.js
@@ -35,3 +35,31 @@ alert("Logout Successful");
 window.location.href="login.html";
 
 });
+
+const saveBtn = document.getElementById("saveProfileBtn");
+
+saveBtn.addEventListener("click", async () => {
+
+const user = auth.currentUser;
+
+if (!user) return;
+
+const newName = document.getElementById("userName").value.trim();
+
+if (newName === "") {
+
+alert("Please enter your name.");
+
+return;
+
+}
+
+await updateProfile(user, {
+displayName: newName
+});
+
+alert("✅ Profile Updated Successfully!");
+
+location.reload();
+
+});
