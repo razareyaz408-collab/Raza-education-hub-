a05a15ace62520c9a0e59f60083443e1d7c2d6c4
razareyaz408-collab <razareyaz408@gmail.com>
2026-07-12 16:44:35 +0530

    Enhance dashboard.js with database functionality
    Added database import and user data saving on auth state change.

--- dashboard.js
+++ dashboard.js
@@ -1,14 +1,31 @@
-import { auth } from "./firebase.js";
+import { auth, database } from "./firebase.js";
 
 import {
 onAuthStateChanged,
 signOut
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
+import {
+ref,
+set
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+
 onAuthStateChanged(auth, (user) => {
 
 if (user) {
 
+set(
+ref(database, "students/" + user.uid),
+{
+name: user.displayName,
+email: user.email,
+overallProgress: 35,
+quranProgress: 20,
+hadithProgress: 60,
+quizScore: 85
+}
+);
+  
 document.getElementById("welcome").innerHTML =
 "👋 Welcome " + (user.displayName || "Student");
 
