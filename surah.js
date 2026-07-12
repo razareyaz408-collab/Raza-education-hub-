868247a3ef587d67beb9929047adae8592b94ea3
razareyaz408-collab <razareyaz408@gmail.com>
2026-07-12 21:25:09 +0530

    Update surah.js

--- surah.js
+++ surah.js
@@ -7,19 +7,48 @@ async function loadSurah() {
 try {
 
 const response = await fetch(
-
-`https://api.alquran.cloud/v1/surah/${id}`
-
+`https://api.alquran.cloud/v1/surah/${id}/editions/quran-uthmani,en.asad`
 );
 
 const result = await response.json();
 
 document.getElementById("surahTitle").innerHTML =
-result.data.englishName;
+arabic.englishName + " (" + arabic.name + ")";
 
 let html = "";
 
-result.data.ayahs.forEach((ayah) => {
+const arabic = result.data[0];
+const english = result.data[1];
+
+arabic.ayahs.forEach((ayah, index) => {
+
+html += `
+
+<div class="card">
+
+<h2 style="text-align:right;font-size:32px;line-height:2;">
+
+${ayah.text}
+
+</h2>
+
+<p style="margin-top:15px;color:#444;">
+
+${english.ayahs[index].text}
+
+</p>
+
+<p>
+
+Ayah ${ayah.numberInSurah}
+
+</p>
+
+</div>
+
+`;
+
+}); NJN
 
 html += `
 
