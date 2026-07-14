77f76c78465c0eff5975a08a723fc4b473e2373a
razareyaz408-collab <razareyaz408@gmail.com>
2026-07-14 16:30:07 +0530

    Update hadith-reader.js

--- hadith-reader.js
+++ hadith-reader.js
@@ -232,3 +232,240 @@ document.body.classList.toggle(
 console.log(
 "✅ Hadith Reader Part 3 Loaded"
 );
+
+// ======================================
+// RAZA EDUCATION HUB
+// HADITH READER SYSTEM
+// PART 4
+// ======================================
+
+
+// ALL HADITH LIST
+
+let hadithArray =
+JSON.parse(
+localStorage.getItem("hadithList")
+) || [];
+
+let currentIndex = 0;
+
+
+// CURRENT HADITH FIND
+
+if(hadith && hadithArray.length){
+
+currentIndex =
+hadithArray.findIndex(
+(item)=>
+JSON.stringify(item)
+===
+JSON.stringify(hadith)
+);
+
+}
+
+
+// NEXT BUTTON
+
+document
+.getElementById("nextBtn")
+?.addEventListener(
+"click",
+()=>{
+
+
+if(currentIndex < hadithArray.length-1){
+
+currentIndex++;
+
+openNextHadith();
+
+}
+
+else{
+
+alert("Last Hadith");
+
+}
+
+
+});
+
+
+
+
+// PREVIOUS BUTTON
+
+document
+.getElementById("prevBtn")
+?.addEventListener(
+"click",
+()=>{
+
+
+if(currentIndex > 0){
+
+currentIndex--;
+
+openNextHadith();
+
+}
+
+else{
+
+alert("First Hadith");
+
+}
+
+
+});
+
+
+
+
+// OPEN HADITH
+
+function openNextHadith(){
+
+
+let newHadith =
+hadithArray[currentIndex];
+
+
+localStorage.setItem(
+"selectedHadith",
+JSON.stringify(newHadith)
+);
+
+
+location.reload();
+
+
+}
+
+
+
+
+// READING COUNT
+
+let readCount =
+Number(
+localStorage.getItem("readCount")
+)||0;
+
+
+if(hadith){
+
+readCount++;
+
+
+localStorage.setItem(
+"readCount",
+readCount
+);
+
+}
+
+
+
+// SHOW STATS
+
+let totalRead =
+document.getElementById("totalRead");
+
+
+if(totalRead){
+
+totalRead.innerHTML =
+"Total Read : "
++
+readCount;
+
+}
+
+
+
+// BOOKMARK COUNT
+
+let bookmarks =
+JSON.parse(
+localStorage.getItem("bookmarks")
+)||[];
+
+
+let bookmarkCount =
+document.getElementById("bookmarkCount");
+
+
+if(bookmarkCount){
+
+bookmarkCount.innerHTML =
+"Bookmarks : "
++
+bookmarks.length;
+
+}
+
+
+
+
+// FAV COUNT
+
+let favourites =
+JSON.parse(
+localStorage.getItem("favourites")
+)
+||[];
+
+
+let favCount =
+document.getElementById("favCount");
+
+
+if(favCount){
+
+favCount.innerHTML =
+"Favourites : "
++
+favourites.length;
+
+}
+
+
+
+// PROGRESS BAR
+
+let progress =
+document.getElementById("progressBar");
+
+
+let info =
+document.getElementById("readingInfo");
+
+
+
+if(progress && hadithArray.length){
+
+
+progress.max =
+hadithArray.length;
+
+
+progress.value =
+currentIndex+1;
+
+
+info.innerHTML =
+`
+Hadith 
+${currentIndex+1}
+of
+${hadithArray.length}
+`;
+
+}
+
+
+console.log(
+"✅ Hadith Reader Part 4 Loaded"
+);
